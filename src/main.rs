46f3fae99ab8f23880e845e75c3aab87c5c9af8d
Aarish Azfar Mirza <aarish98@users.noreply.github.com>
2023-04-24 15:49:45 +0500

    get user input for grid size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
+//initialize our Minesweeper Grid
+fn init_grid(){
+    println!("Hello! Give a number for a nxn grid.");
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => println!("Creating a grid of size {} x {}", i , i),
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+}
+
 fn main() {
+
+    init_grid();
+    
     println!("Hello, world!");
 }
