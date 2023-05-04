5cd34bcef4b27a672175825015cab6f0a8251313
Aarish Azfar Mirza <aarish98@users.noreply.github.com>
2023-05-05 01:04:44 +0500

    add open cells

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-use std::io;
+use std::{io, collections::btree_map::Range};
 use rand::{thread_rng, Rng};
 
-type Position = (usize, usize);
+type Position = (i32, i32);
 
 struct MineSweeper {
-    width: usize,
-    height: usize,
+    width: i32,
+    height: i32,
     open_cells: Vec<Position>,
     mines: Vec<Position>,
     flagged_cells: Vec<Position>,
 }
 
 impl MineSweeper {
-    pub fn new(width: usize, height: usize, mine_count: usize) -> MineSweeper {
+    pub fn new(width: i32, height: i32, mine_count: usize) -> MineSweeper {
         MineSweeper {
             width,
             height,
-            open_cells: Vec::new(),
+            open_cells: {
+                let mut open_cells: Vec<Position>  =  Vec::new();
+                for i in 0..width {
+                    for j in 0..height{
+                        open_cells.push((i,j));
+                    }
+                }
+                open_cells
+            },
             mines: {
                 let mut mines:Vec<Position> = Vec::new();
 
@@ -32,7 +40,7 @@ impl MineSweeper {
 }
 
 //creates a random number between 0 and max
-pub fn random_number(max: usize) -> usize {
+pub fn random_number(max: i32) -> i32 {
     let mut rng = thread_rng();
     rng.gen_range(0..max)
   }
@@ -56,8 +64,13 @@ fn main() {
 
     let grid_size = get_user_input();
     
+
     if grid_size > 0 {
         let mut grid  =  [[0u8; 4]; 6];
+
+        let mut mineSweeper = MineSweeper::new(grid_size, grid_size, (0.25_f64 * (grid_size* grid_size) as f64).floor() as usize );
+        
+
 
         println!("Hello, world!: {}", grid_size);
     }
